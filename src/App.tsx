9a3b0406a14232391c86ab6657c73aba5f668809
DG-7D <96519093+DG-7D@users.noreply.github.com>
2026-04-16 20:40:41 +0900

    ればー

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const leverDigits = 10;
+
+export function App() {
+  const [leverValues, setLeverValues] = React.useState(Array(leverDigits).fill(0));
+  return (
+    <div className="app">
+      {leverValues.map(
+        (value, index) => <Lever key={index} value={value} setValue={(newValue) => {
+          setLever(index, newValue);
+        }} />
+      )}
+    </div>
+  );
+  function setLever(digit: number, newValue: number) {
+    const newLeverValues = [...leverValues];
+    newLeverValues[digit] = newValue;
+    setLeverValues(newLeverValues);
+  }
+}
+
+function Lever({ value, setValue }: { value: number, setValue: (newValue: number) => void }) {
+  return (
+    <div className="lever">
+      <button onClick={() => value === 0 || setValue(value - 1)}>-</button>
+      <button onClick={() => value === 9 || setValue(value + 1)}>+</button>
+      <input type="number" min={0} max={9} value={value} onChange={(e) => setValue(parseInt(e.target.value))} />
+      <input type="range" min={0} max={9} value={value} onChange={(e) => setValue(parseInt(e.target.value))} />
+    </div>
+  );
+}
+
+
+export default App;
