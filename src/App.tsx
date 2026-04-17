a4539e7c8b0c7d07d66d251855e5bd7676394dd2
DG-7D <96519093+DG-7D@users.noreply.github.com>
2026-04-17 14:38:49 +0900

    足し引きできるようになった?

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const leverDigits = 10;
+const dialLDigits = 11;
+const dialRDigits = 21;
 
 export function App() {
   const [leverValues, setLeverValues] = React.useState(Array(leverDigits).fill(0));
+  const [dialLValues, setDialLValues] = React.useState(Array(dialLDigits).fill(0));
+  const [dialRValues, setDialRValues] = React.useState(Array(dialRDigits).fill(0));
+  const [clutch, setClutch] = React.useState(0);
   return (
     <div className="app">
+      <button onClick={() => crank(false)}>+</button>
+      <button onClick={() => crank(true)}>-</button>
       {
         leverValues.map(
           (value, index) => <Lever key={index} value={value} setValue={
@@ -15,12 +22,54 @@ export function App() {
           } />
         )
       }
+      {
+        dialRValues.map(
+          (value, index) => <div className="dialR" key={index}>{value}</div>
+        )
+      }
+      <div>{(() => {
+        switch (clutch) {
+          case 0: return "N";
+          case 1: return "×";
+          case -1: return "÷";
+        }
+      })()}</div>
+      {
+        dialLValues.map(
+          (value, index) => <div className="dialL" key={index}>{value}</div>
+        )
+      }
     </div>
   );
   function setDigit(values: number[], setValues: React.Dispatch<React.SetStateAction<number[]>>, digit: number, newValue: number) {
     const newValues = [...values];
     newValues[digit] = newValue;
     setValues(newValues);
+  }
+  function addValue(a: number[], b: number[], sub: boolean = false, maxDigits: number = Math.max(a.length, b.length)) {
+    const sign = sub ? -1 : 1;
+    let sum = [...a];
+    let carry = 0;
+    for (let i = 0; i < maxDigits; i++) {
+      const digit = (a[i] || 0) + sign * (b[i] || 0) + carry;
+      sum[i] = (digit + 10) % 10;
+      carry = Math.floor(digit / 10);
+    }
+    return { sum, carry };
+  }
+
+  function crank(sub: boolean = false) {
+    const { sum, carry } = addValue(dialRValues, leverValues, sub, 13); // TODO: maxDigitsは桁送りしたら変わる
+    setDialRValues(sum.slice(0, dialRDigits));
+    console.log('Carry:', carry);
+
+    const sign = sub ? -1 : 1;
+    if (clutch === 0) {
+      setClutch(sign);
+      setDialLValues(addValue(dialLValues, [1]).sum);
+    } else {
+      setDialLValues(addValue(dialLValues, [sign * clutch]).sum);
+    }
   }
 }
 
